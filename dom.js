7df6349354c6a96bc79d029df5d05c111aa88aa2
mamun1336 <abmamun504@gmail.com>
2020-06-02 17:24:59 +0600

    html

---
+++ dom.js
@@ -0,0 +1,8 @@
+//EXAMINE THE DOCUMENT OBJECT//
+
+console.dir(document);
+console.log(document.domain);
+console.log(document.title);
+// document.title = 123;
+console.log(document.doctype);
+console.log(document.head);
